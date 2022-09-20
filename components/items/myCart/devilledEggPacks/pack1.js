5e0b77019c3732945742e991da1e097642082f95
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 20:48:50 +0100

    Add files via upload

---
+++ components/items/myCart/devilledEggPacks/pack1.js
@@ -0,0 +1,25 @@
+const egg1 =()=>{
+    return`<div class ="food17">
+<button type="button" class="clearDE1">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/devilled_eggs.png"  id="productAlign" alt="food17" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Devilled Egg
+<br/>
+<span class="miniTxt">On Train</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$9.00
+</p>
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`;
+}
+export default egg1;
