a0120427112e5ef2a20c6fce0fe70f18bdbc8979
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:48:43 +0100

    Add files via upload

---
+++ components/items/myCart/wheatBallsPacks/pack1.js
@@ -0,0 +1,24 @@
+const wheat1 =()=>{
+    return `<div class ="food9">
+<button type="button" class="clearWB1">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/wheat.png" id="productAlign" alt="food9" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Wheat Balls
+<br/>
+<span class="miniTxt">Without Soup</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$2.00</p>
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`;
+}
+export default wheat1;
---
+++ components/items/myCart/wheatBallsPacks/pack3.js
@@ -0,0 +1,24 @@
+const wheat3 =()=>{
+    return `<div class ="food9">
+<button type="button" class="clearWB3">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/wheat.png" id="productAlign" alt="food9" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Wheat Balls
+<br/>
+<span class="miniTxt">Without Soup</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$5.00</p>
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+</div>`;
+}
+export default wheat3;
