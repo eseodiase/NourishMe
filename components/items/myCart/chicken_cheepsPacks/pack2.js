dbe2296eb2ad53bc4bd176c3b9d52fce42cf0ace
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 20:42:15 +0100

    Add files via upload

---
+++ components/items/myCart/chicken_cheepsPacks/pack2.js
@@ -0,0 +1,28 @@
+const c_cheeps2 =()=>{
+    return`<div class ="food15">
+<button type="button" class="clearBCC2">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/barbecue_chicken.png" 
+id="productAlign" alt="food15" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Barbecue Chicken 
+<br/>
+<span class="miniTxt">
+With Cheeps 
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$14.00
+</p>
+
+<p id="productAlign">
+<strong>+2</strong>
+</p>
+</div>`;
+}
+export default c_cheeps2;
---
+++ components/items/myCart/chicken_cheepsPacks/pack3.js
@@ -0,0 +1,28 @@
+const c_cheeps3 =()=>{
+    return`<div class ="food15">
+<button type="button" class="clearBCC3">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/barbecue_chicken.png" 
+id="productAlign" alt="food15" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Barbecue Chicken 
+<br/>
+<span class="miniTxt">
+With Cheeps 
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$20.00
+</p>
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+</div>`;
+}
+export default c_cheeps3;
