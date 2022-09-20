3fc723ba70e9da4bdd09133ea326bc4e2fd9725f
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 20:52:28 +0100

    Add files via upload

---
+++ components/items/myCart/friedChikenPacks/pack2.js
@@ -0,0 +1,27 @@
+const chicken2 =()=>{
+    return`<div class ="food12">
+<button type="button" class="clearDFC2">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/deep-fried_chicken.png" id="productAlign" alt="food12" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Chicken
+<br/>
+<span class="miniTxt">
+Deep Fried
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$9.00
+</p>
+
+<p id="productAlign">
+<strong>+2</strong>
+</p>
+</div>`;
+}
+export default chicken2;
