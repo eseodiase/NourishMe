5a46ee53fead9b8243623572c863e5a8687149dc
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:28:19 +0100

    Add files via upload

---
+++ components/items/myCart/pepperSoupPacks/pack1.js
@@ -0,0 +1,27 @@
+const CatfishPack1 =()=>{
+    return `<div class ="food2">
+    <button type="button" class="clearCFPsoup1">
+    <i class="fa fa-trash" id="trash"></i>
+    </button>
+    
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/fish_soup.png" id="productAlign" alt="food2" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Pepper Soup
+<br />
+<span class="miniTxt">
+With Catfish    
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$5.00
+</p>  
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`;
+}
+export default CatfishPack1;
---
+++ components/items/myCart/pepperSoupPacks/pack3.js
@@ -0,0 +1,26 @@
+const CatfishPack3 =()=>{
+    return `<div class ="food2">
+    <button type="button" class="clearCFPsoup3">
+    <i class="fa fa-trash" id="trash"></i>
+    </button>
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/fish_soup.png" id="productAlign" alt="food2" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Pepper Soup
+<br />
+<span class="miniTxt">
+With Catfish    
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$14.00
+</p>  
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+</div>`;
+}
+export default CatfishPack3;
