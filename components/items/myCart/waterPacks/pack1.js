211416f75faa3449fab071c99e04dce5e658c44f
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:46:13 +0100

    Add files via upload

---
+++ components/items/myCart/waterPacks/pack1.js
@@ -0,0 +1,26 @@
+const water1 =()=>{
+    return`<div class ="food14">
+<button type="button" class="clearSW1">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/water.png"
+id="productAlign" alt="food14" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Sparkling Water
+<br/>
+<span class="miniTxt">Demo</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$1.00
+</p>
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`
+}
+export default water1;
---
+++ components/items/myCart/waterPacks/pack2.js
@@ -0,0 +1,25 @@
+const water2 =()=>{
+    return`<div class ="food14">
+<button type="button" class="clearSW2">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/water.png" id="productAlign" alt="food14" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Sparkling Water
+<br/>
+<span class="miniTxt">Demo</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$2.00
+</p>
+
+<p id="productAlign">
+<strong>+2</strong>
+</p>
+</div>`
+}
+export default water2;
