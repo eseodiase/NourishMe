3ccb723d7cab9855ac18497cda2ca7f5bc88e74b
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:08:26 +0100

    Add files via upload

---
+++ components/items/myCart/fruitSaladPacks/pack1.js
@@ -0,0 +1,25 @@
+const fruitSalad1 =()=>{
+    return`<div class ="food11">
+<button type="button" class="clearFS1">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/fruitSalad.png" id="productAlign" alt="food11" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Fruit Salad
+<br/>
+<span class="miniTxt">Refrigerated</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$3.00</p>
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`;
+
+}
+export default fruitSalad1;
---
+++ components/items/myCart/fruitSaladPacks/pack3.js
@@ -0,0 +1,26 @@
+const fruitSalad3 =()=>{
+    return`<div class ="food11">
+<button type="button" class="clearFS3">
+<i class="fa fa-trash" id="trash"></i>
+</button>
+
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/fruitSalad.png"
+id="productAlign" alt="food11" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Fruit Salad
+<br/>
+<span class="miniTxt">Refrigerated</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$7.00</p>
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+</div>`;
+
+}
+export default fruitSalad3;
