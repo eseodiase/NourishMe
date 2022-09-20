4f5bbf944991be3401a1ff63f49b092cfd80e30e
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:33:19 +0100

    Add files via upload

---
+++ components/items/myCart/redWinePacks/pack1.js
@@ -0,0 +1,27 @@
+const redWine1=()=>{
+    return`<div class ="food3">
+    <button type="button" class="clearRw1">
+    <i class="fa fa-trash" id="trash"></i>
+    </button>
+    
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/red_wine.png" id="productAlign" alt="food3" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Red Wine
+<br />
+<span class="miniTxt">
+Demo
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$8.00
+</p>
+
+<p id="productAlign">
+<strong>+1</strong>
+</p>
+</div>`;
+}
+export default redWine1;
---
+++ components/items/myCart/redWinePacks/pack3.js
@@ -0,0 +1,28 @@
+const redWine3=()=>{
+    return`<div class ="food3">
+    <button type="button" class="clearRw3">
+    <i class="fa fa-trash" id="trash"></i>
+    </button>
+    
+<img src="https://eseodiase.github.io/NourishMe/components/items/images/red_wine.png"
+id="productAlign" alt="food3" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Red Wine
+<br />
+<span class="miniTxt">
+Demo
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$22.00
+</p>
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+</div>`;
+}
+export default redWine3;
