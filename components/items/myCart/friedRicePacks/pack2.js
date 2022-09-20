bae83038438849426dec0b27d34a1b2ca2b04e56
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-20 21:05:10 +0100

    Add files via upload

---
+++ components/items/myCart/friedRicePacks/pack2.js
@@ -0,0 +1,29 @@
+const pack2 =()=>{
+    return `<div class ="food1">
+    
+ <button type="button" class="clear2">
+ <i class="fa fa-trash" id="trash"></i>
+ </button>
+ 
+ <img src="https://eseodiase.github.io/NourishMe/components/items/images/fried_rice.png" id="productAlign" alt="food1" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Fried Rice
+<br />
+<span class="miniTxt">
+With Chicken    
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$13.00
+</p>
+
+<p id="productAlign">
+<strong>+2</strong>
+</p>
+
+ </div>`
+}
+export default pack2;
---
+++ components/items/myCart/friedRicePacks/pack3.js
@@ -0,0 +1,29 @@
+const pack3 =()=>{
+    return `<div class ="food1">
+    
+ <button type="button" class="clear3">
+ <i class="fa fa-trash" id="trash"></i>
+ </button>
+ 
+ <img src="https://eseodiase.github.io/NourishMe/components/items/images/fried_rice.png" id="productAlign" alt="food1" class="itemsImg"/>
+
+<p class ="title" id="productAlign">
+Fried Rice
+<br />
+<span class="miniTxt">
+With Chicken    
+</span>
+</p>
+<br/>
+
+<p id="productAlign" style="float:right;">
+$20.00
+</p>
+
+<p id="productAlign">
+<strong>+3</strong>
+</p>
+
+ </div>`
+}
+export default pack3;
