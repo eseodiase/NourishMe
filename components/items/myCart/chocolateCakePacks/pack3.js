69357f62c14964258fe537b318a917ffc056f286
eseodiase <50231281+eseodiase@users.noreply.github.com>
2022-09-23 15:04:04 +0100

    pack3.js

--- components/items/myCart/chocolateCakePacks/pack3.js
+++ components/items/myCart/chocolateCakePacks/pack3.js
@@ -4,7 +4,7 @@ const chocolateCake3 =()=>{
 <i class="fa fa-trash" id="trash"></i>
 </button>
 
-<img src="https://eseodiase.github.io/NourishMe/components/items/images/chocolate_cake.png"
+<img src="images/chocolate_cake.png"
 id="productAlign" alt="food6" class="itemsImg"/>
 
 <p class ="title" id="productAlign">
